41990724a7a73d354cc31b86d66fb1c6714ccd30
Sangwon Lee <samlee0326@gmail.com>
2024-12-10 11:28:53 +0900

    Add files via upload

--- daangn/settings.py
+++ daangn/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +111,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+   "D:\DevCourse\project1-prod\daangn\category\static",
+]
