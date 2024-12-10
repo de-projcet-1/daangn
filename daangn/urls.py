4c9234b88483978c9ac08bcf084e692ca394d7b4
Sangwon Lee <samlee0326@gmail.com>
2024-12-10 11:28:16 +0900

    Add files via upload

--- daangn/urls.py
+++ daangn/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from category import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('region/', views.user_type_select, name='user_type_select'),
+    path('region/<str:user_type>/', views.region_select, name='region_select'),
+    path('region/<str:user_type>/<str:TOWN>/', views.region_detail_view, name='region_detail_view'),
 ]
+
